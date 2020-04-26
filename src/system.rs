337311757db69621d1ec1718184516d86ddef849
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-04-26 16:34:54 +0900

    :sparkles: calculate prg/chr rom sizes

--- src/system.rs
+++ src/system.rs
@@ -9,7 +9,7 @@ pub enum Ines {
 }
 
 pub fn read_nes(path: String) -> Ines {
-  println!("Load: {}", path);
+  println!("Target: {}", path);
   let mut file = File::open(path).unwrap();
   let mut buffer: Vec<u8> = Vec::new();
 
@@ -30,8 +30,16 @@ pub fn header_process(path: String) -> Result<(), String> {
       if buffer[2] != 0x53 { return Err("expect 0x53".to_string()); }
       if buffer[3] != 0x1a { return Err("expect 0x1a".to_string()); }
 
-      let prg_size = buffer[4] as usize;
-      let chr_size = buffer[5] as usize;
+      let prg_block = buffer[4] as usize;
+      let chr_block = buffer[5] as usize;
+      let prg_bytes = prg_block * 0x4000;
+      let chr_bytes = chr_block * 0x2000;
+
+      let header = 16;
+      let prg_addr = header;
+      let chr_addr = header + prg_addr + prg_bytes;
+      println!("PRG-ROM bytes: \x1b[38;5;51m0x{:>08x} ~ 0x{:>08x}\x1b[m", prg_addr, prg_addr + prg_bytes);
+      println!("CHR-ROM bytes: \x1b[38;5;51m0x{:>08x} ~ 0x{:>08x}\x1b[m", chr_addr, chr_addr + chr_bytes);
     },
 
     _ => panic!("Invalid file type")
