b7106e50359cdaec4f7f899581ba79b59bf7f1ea
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-04-26 10:36:18 +0900

    :sparkles: implement cpu::new

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,10 +1,34 @@
-// http://pgate1.at-ninja.jp/NES_on_FPGA/nes_cpu.htm#register
-struct Cpu {
+ struct Cpu {
+  // accumulator
   a: i8,
-  x: i8,
-  y: i8,
+  // index
+  x: i8, y: i8,
+  // program counter
   pc: i16,
+  // stack pointer
   sp: i8,
+  /*
+   * processer status
+   * 7: Negative,
+   * 6: oVerflow,
+   * 4: Break,
+   * 3: Decimal,
+   * 2: Interrupt,
+   * 1: Zero,
+   * 0: Carry
+   */
   p: i8
+}
+
+impl Cpu {
+  fn new() -> Self {
+    Self {
+      a: 0,
+      x: 0, y: 0,
+      pc: 0,
+      sp: 0,
+      p: 0
+    }
+  }
 }
 
